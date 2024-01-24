ae650cf45bb8f3490612e4a89f8b70f3af238b6e
Alex Caicedo <chevyagcr@gmail.com>
2024-01-24 07:35:04 -0500

    1er Commit: Se creo las carpetas Models,ViewModels y views, junto con una clase ApiCaller que llamará a la API y uan clase artistas que tiene todos los métodos que usa la API, se creo las páginas principales que se va a usar en el proyecto.

---
+++ Models/AC_ApiCaller.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace CaicedoRamos_Progreso3.Controladores
+{
+    public class ApiCaller
+    {
+        private static readonly HttpClient client = new HttpClient();
+
+        public async Task<AC_Artist.Root> GetArtistData(string artistName)
+        {
+            string url = $"https://api.musixmatch.com/ws/1.1/artist.search?apikey=220c88aac709236296f7d4ff683d403b&q_artist={artistName}&page_size=30";
+            string json = await GetJsonFromAPI(url);
+            AC_Artist.Root artists = JsonSerializer.Deserialize<AC_Artist.Root>(json);
+            return artists;
+        }
+
+        private async Task<string> GetJsonFromAPI(string url)
+        {
+            HttpResponseMessage response = await client.GetAsync(url);
+            response.EnsureSuccessStatusCode();
+            string responseBody = await response.Content.ReadAsStringAsync();
+            return responseBody;
+        }
+    }
+}
---
+++ Views/ResultsPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace CaicedoRamos_Progreso3.Views;
+
+public partial class ResultsPage : ContentPage
+{
+	public ResultsPage()
+	{
+		InitializeComponent();
+	}
+}
